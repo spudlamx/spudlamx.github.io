8d7dbec052135593a837a80e6666b55290c1915c
Spencer Lamoreaux <lam21016@byui.edu>
2023-10-05 20:30:22 -0600

    style

---
+++ wdd230/chamber/scripts/chamber.js
@@ -0,0 +1,14 @@
+const options = { year: "numeric" };
+document.getElementById("currentyear").textContent =
+  new Date().toLocaleDateString("en-US", options);
+
+document.getElementById(
+  "lastModified"
+).innerHTML = `Last Modification: ${document.lastModified}`;
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
