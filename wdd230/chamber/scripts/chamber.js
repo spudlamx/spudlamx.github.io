f56c58d0d1fe94b503488bd97c331634bcda277f
Spencer Lamoreaux <lam21016@byui.edu>
2023-10-07 21:06:27 -0600

    discard unneeded

--- wdd230/chamber/scripts/chamber.js
+++ wdd230/chamber/scripts/chamber.js
@@ -2,13 +2,13 @@ const options = { year: "numeric" };
 document.getElementById("currentyear").textContent =
   new Date().toLocaleDateString("en-US", options);
 
-document.getElementById(
-  "lastModified"
-).innerHTML = `Last Modification: ${document.lastModified}`;
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+// document.getElementById(
+//   "lastModified"
+// ).innerHTML = `Last Modification: ${document.lastModified}`;
+// const hamButton = document.querySelector('#menu');
+// const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-  navigation.classList.toggle('open');
-  hamButton.classList.toggle('open');
-});
+// hamButton.addEventListener('click', () => {
+//   navigation.classList.toggle('open');
+//   hamButton.classList.toggle('open');
+// });
