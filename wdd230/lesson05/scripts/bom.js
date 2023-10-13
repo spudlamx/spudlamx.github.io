de6d9ce8ab81acb4f0630e51db0af597fc55cbb6
Spencer Lamoreaux <lam21016@byui.edu>
2023-10-12 20:05:06 -0600

    bom JS

---
+++ wdd230/lesson05/scripts/bom.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+  if (input.value !== '') {
+
+    const listItem = document.createElement('li');
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+
+    listItem.textContent = input.value;
+
+    listItem.appendChild(deleteButton);
+    
+    list.appendChild(listItem);
+    
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(listItem);
+    });
+
+    input.focus();
+    
+    input.value = '';
+  } else {
+    // If input is blank, provide a message
+    alert('Please enter a chapter.');
+    input.focus();
+  }
+});
+
+  
