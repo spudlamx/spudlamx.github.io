8070da3fd46565898620fdeb68da0e6015df03d2
Spencer Lamoreaux <lam21016@byui.edu>
2023-11-09 21:09:13 -0700

    syntax

--- wdd230/scripts/links.js
+++ wdd230/scripts/links.js
@@ -1,32 +1,30 @@
 const baseURL = "https://spudlamx.github.io/wdd230/";
-const linksURL = "https://spudlamx.github.io/wdd230/data/links.json";
+const linksURL = "/Users/spencerlamoreaux/Documents/BYU-I/2023 Fall/Web design/wdd230/wdd230/data/links.json";
 
 async function getLinks() {
-  const response = await fetch(linksURL);
-  const data = await response.json();
-  displayLinks(data);
-}
-
-function displayLinks(weeks) {
-  // Assuming you have an HTML container with id 'links-container' to display the links
-  const linksContainer = document.getElementById('links-container');
-
-  weeks.forEach(week => {
-    const weekElement = document.createElement('div');
-    weekElement.innerHTML = `<h3>Week ${week.lesson}</h3>`;
-    
-    const linksList = document.createElement('ul');
-
-    week.links.forEach(link => {
-      const linkItem = document.createElement('li');
-      linkItem.innerHTML = `<a href="${link.url}" target="_blank">${link.title}</a>`;
-      linksList.appendChild(linkItem);
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+  }
+  
+  function displayLinks(weeks) {
+    const linksContainer = document.getElementById('links-container');
+  
+    weeks.forEach(week => {
+      const weekElement = document.createElement('div');
+      weekElement.innerHTML = `<h3>Week ${week.lesson}</h3>`;
+      
+      const linksList = document.createElement('ul');
+  
+      week.links.forEach(link => {
+        const linkItem = document.createElement('li');
+        linkItem.innerHTML = `<a href="${link.url}" target="_blank">${link.title}</a>`;
+        linksList.appendChild(linkItem);
+      });
+  
+      weekElement.appendChild(linksList);
+      linksContainer.appendChild(weekElement);
     });
-
-    weekElement.appendChild(linksList);
-    linksContainer.appendChild(weekElement);
-  });
-}
-
-// Uncomment the line below to test and log the data to the console
-// getLinks();
+  }
+  
+  getLinks();
