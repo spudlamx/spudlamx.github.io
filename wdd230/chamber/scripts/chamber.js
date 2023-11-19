40921e68b72afdb8b8fc86e3f2b899ee60496b0f
Spencer Lamoreaux <lam21016@byui.edu>
2023-11-18 17:21:07 -0700

    forcast

--- wdd230/chamber/scripts/chamber.js
+++ wdd230/chamber/scripts/chamber.js
@@ -63,45 +63,45 @@ modeButton.addEventListener("change", () => {
 // Get the temperature and wind speed values from the data attributes
 
 
-// Get live temperature and wind speed values (replace these with actual values)
-var liveTemperature = 49; // in °F
-var liveWindSpeed = 12; // in mph
+// // Get live temperature and wind speed values (replace these with actual values)
+// var liveTemperature = 49; // in °F
+// var liveWindSpeed = 12; // in mph
 
-// Update the data attributes with the live values
-document.getElementById("temperature").setAttribute("data-value", liveTemperature);
-document.getElementById("windspeed").setAttribute("data-value", liveWindSpeed);
+// // Update the data attributes with the live values
+// document.getElementById("temperature").setAttribute("data-value", liveTemperature);
+// document.getElementById("windspeed").setAttribute("data-value", liveWindSpeed);
 
-document.getElementById("temperature").textContent = liveTemperature;
-document.getElementById("windspeed").textContent = liveWindSpeed;
+// document.getElementById("temperature").textContent = liveTemperature;
+// document.getElementById("windspeed").textContent = liveWindSpeed;
 
-var temperatureInput = parseFloat(
-  document.getElementById("temperature").getAttribute("data-value")
-);
-var windSpeedInput = parseFloat(
-  document.getElementById("windspeed").getAttribute("data-value")
-);
+// var temperatureInput = parseFloat(
+//   document.getElementById("temperature").getAttribute("data-value")
+// );
+// var windSpeedInput = parseFloat(
+//   document.getElementById("windspeed").getAttribute("data-value")
+// );
 
-// Calculate the wind chill
-function calculateWindChill(temperature, windSpeed) {
-  // Check if temperature is less than or equal to 50°F and wind speed is greater than 3.0 mph
-  if (temperature <= 50 && windSpeed > 3.0) {
-    // Calculate wind chill using the formula
-    var windChill =
-      35.74 +
-      0.6215 * temperature -
-      35.75 * Math.pow(windSpeed, 0.16) +
-      0.4275 * temperature * Math.pow(windSpeed, 0.16);
-    return windChill.toFixed(1); // Round to one decimal place
-  } else {
-    return "N/A"; // Wind chill not applicable
-  }
-}
+// // Calculate the wind chill
+// function calculateWindChill(temperature, windSpeed) {
+//   // Check if temperature is less than or equal to 50°F and wind speed is greater than 3.0 mph
+//   if (temperature <= 50 && windSpeed > 3.0) {
+//     // Calculate wind chill using the formula
+//     var windChill =
+//       35.74 +
+//       0.6215 * temperature -
+//       35.75 * Math.pow(windSpeed, 0.16) +
+//       0.4275 * temperature * Math.pow(windSpeed, 0.16);
+//     return windChill.toFixed(1); // Round to one decimal place
+//   } else {
+//     return "N/A"; // Wind chill not applicable
+//   }
+// }
 
-var windChillValue = calculateWindChill(temperatureInput, windSpeedInput);
+// var windChillValue = calculateWindChill(temperatureInput, windSpeedInput);
 
-// Display the wind chill value on your web page
-document.getElementById("windchill").textContent =
-  "Wind Chill: " + windChillValue + " °F";
+// // Display the wind chill value on your web page
+// document.getElementById("windchill").textContent =
+//   "Wind Chill: " + windChillValue + " °F";
 
 
 
